23c2c4c95e1801d4a5ee309c8228abce4452901e
omsaggau <omsaggau@gmail.com>
2015-02-25 12:52:14 +0100

    Fungerende getRoom

--- Kalenderserver/src/core/SCore.java
+++ Kalenderserver/src/core/SCore.java
@@ -3,7 +3,11 @@ package core;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class SCore {
 	DBConnector dbc;
@@ -13,26 +17,52 @@ public class SCore {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		//System.out.println(getRoom(19,new Date(0)));
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		Date d = null;
+		try {
+			d = new java.sql.Date(sdf.parse("2015/02/25").getTime());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(d.toString());
+		System.out.println(getRoom(8,d,java.sql.Time.valueOf("11:00:00"),java.sql.Time.valueOf("13:00:00")));
 	}
 	
-	public String getRoom(int size,Date start,Date slutt){
+	public ArrayList<List<Object>> getRoom(int size,Date dato,Time start, Time slutt){
+		//Gir tilbake en liste med lister over rom som er ledig til den tid
+		
 		ResultSet rs1;
 		ResultSet rs2;
-		ArrayList<ResultSet> rooms;
 		try {
 			String statement = "SELECT 'romnr' FROM 'rom' WHERE 'plass' >= "+size;
 			System.out.println(statement);
-			rs1 = dbc.executeSQL("SELECT romnr FROM rom WHERE plass >= "+size);
+			rs1 = dbc.executeSQL("SELECT romnr,plass FROM rom WHERE plass >= "+size);
 			ArrayList<List<Object>> potentialRooms = resToList(rs1);
+			String rooms = ",";
 			for (List<Object> o:potentialRooms){
-				
+				rooms+= o.get(0).toString()+",";
 			}
-			//rs2 = dbc.getQueryCondition("reservasjon", "romNr",, "romnr", "start","slutt");
-			return resToList(rs1).toString();
+			rooms = rooms.substring(1,rooms.length()-1);
+			System.out.println(rooms);
+			rs2= dbc.executeSQL("SELECT romNr FROM reservasjon WHERE romNr in ("+rooms+") AND dato BETWEEN '"+dato+" 00:00:00' AND '"+dato+" 00:00:00' AND start BETWEEN '"+start+"' AND '"+slutt+"' AND slutt BETWEEN '"+start+"' AND '"+slutt+"'");
+			ArrayList<List<Object>> invalidRooms = resToList(rs2);
+			System.out.println(potentialRooms);
+			ArrayList<List<Object>> tobeRemoved = new ArrayList<List<Object>>();
+			
+			for (List<Object> o:potentialRooms){
+				for (List<Object> i:invalidRooms){
+					if (o.get(0).toString().equals(i.get(0).toString())){
+						tobeRemoved.add(o);
+					}
+				}
+			}
+			potentialRooms.removeAll(tobeRemoved);
+			System.out.println(potentialRooms);
+			return potentialRooms;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return "Ingen ledig da.";
+			return null;
 		}
 	}
 	
