bd5ce8fd7774841ce2e79d0bd11b8cce5c990279
stefanab <sasb_94@hotmail.com>
2015-02-26 14:04:29 +0100

    calendarview oppdate

---
+++ Kalenderprogram/src/gui/CalendarViewController.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class CalendarViewController {
+
+	@FXML private ListView moteavtaler;
+	
+	public void editMandag(ActionEvent event) {
+	}
+	
+}
